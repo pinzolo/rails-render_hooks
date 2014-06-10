08cab6fd3b76b564e67342589df28c68019a9e3a
pinzolo <pinzolo@gmail.com>
2014-06-10 10:56:58 +0900

    Initial commit

---
+++ lib/rails/render_hooks.rb
@@ -0,0 +1,7 @@
+require "rails/render_hooks/version"
+
+module Rails
+  module RenderHooks
+    # Your code goes here...
+  end
+end
---
+++ lib/rails/render_hooks/version.rb
@@ -0,0 +1,5 @@
+module Rails
+  module RenderHooks
+    VERSION = "0.0.1"
+  end
+end
